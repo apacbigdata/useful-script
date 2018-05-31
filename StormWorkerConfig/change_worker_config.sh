2bbb07e9135137dc72c53a4a59f2dea392a721c8
xiopeter3345 <xiopeter3345@outlook.com>
2018-05-31 15:45:38 +0800

    add script to change storm worker log4j properties
    add script to change storm worker log4j properties

---
+++ StormWorkerConfig/change_worker_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if test -d /tmp/changeworkerconfig 
+then
+  rm -rf /tmp/changeworkerconfig
+fi
+
+mkdir /tmp/changeworkerconfig
+cd /tmp/changeworkerconfig
+
+wget -q -O /tmp/changeworkerconfig/change_worker_config.py https://raw.githubusercontent.com/apacbigdata/useful-script/master/change_worker_config.py
+
+chmod u+x /tmp/changeworkerconfig/change_worker_config.py
+
+sudo ./change_worker_config.py
